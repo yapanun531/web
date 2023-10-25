c2f542ef5259491315d6fd59d0e84c0f51a3b29f
mnshm8231 <111236191+mnshm8231@users.noreply.github.com>
2023-10-25 09:51:44 +0800

    LiYuan

---
+++ app/LiYuan/useProduct.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import app from "@/app/_firebase/Config"
+import { useEffect, useState } from "react";
+
+function useProducts() {
+  const db = getFirestore(app);
+  const [products, setProducts] = useState<{ desc: string, price: number }[]>([
+    { desc: "iPad", price: 20000 },
+    { desc: "iPhone 8", price: 20000 },
+    { desc: "iPhone X", price: 30000 }
+  ])
+  // fecthData();
+  useEffect(() => {
+    async function fetchData() {
+      let data: { desc: string, price: number }[] = [];
+      const querySnapshot = await getDocs(collection(db, "LiYuan2"));
+      querySnapshot.forEach((doc) => {
+        data.push({ desc: doc.data().desc, price: doc.data().price })
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+      setProducts(() => [...data]);
+    }
+    fetchData();
+  }, [db]);
+
+  return [products, setProducts] as const;
+
+}
+export default useProducts;

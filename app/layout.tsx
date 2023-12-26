9e1fa560e2707630a171ac512f850a1f347d6a0d
yapanun531 <a20030531@gmail.com>
2023-12-26 23:35:24 +0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import ThemeRegistry from './_theme/theme.registory'
 import { AuthContext, AuthContextProvider } from './account/AuthContext'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import "@fortawesome/fontawesome-free/css/all.min.css"
-import React, { useContext } from 'react'
-import { useRouter } from 'next/navigation';
+import React from 'react'
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +17,6 @@ export const metadata: Metadata = {
 }
 
 export function Icon() {
-  const router = useRouter();
-
   const sss = {
     display: 'flex',
     paddingBottom: '5px'
@@ -37,14 +35,18 @@ export function Icon() {
     if (AuthContext.displayName === "普通") {
       return (
         <div style={sss}>
-          <i className="fas fa-circle-user fa-3x" onClick={() => router.push("/account")}></i>
+          <Link href="/account">
+            <i className="fas fa-circle-user fa-3x"></i>
+          </Link>
           <h5 style={sssss}>使用者 {AuthContext.displayName}，您好</h5>
         </div>
       );
     } else {
       return (
         <div style={sss}>
-          <i className="fas fa-circle-user fa-3x" onClick={() => router.push("/account")}></i>
+          <Link href="/account">
+            <i className="fas fa-circle-user fa-3x"></i>
+          </Link>
           <h5 style={ssss}>商家 {AuthContext.displayName} , 您好</h5>
         </div>
       );
@@ -52,7 +54,9 @@ export function Icon() {
   } else {
     return (
       <div style={sss}>
-        <i className="fas fa-circle-user fa-3x" onClick={() => router.push("/account")}></i>
+        <Link href="/account">
+          <i className="fas fa-circle-user fa-3x"></i>
+        </Link>
         <h5 style={ssss}>登入/註冊</h5>
       </div>
     );

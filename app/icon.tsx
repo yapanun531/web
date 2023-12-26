5e124611d969ce72b4b0bbb9e36022e213f6071c
yapanun531 <130235119+yapanun531@users.noreply.github.com>
2023-12-26 20:01:34 +0800

    Update icon.tsx

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,58 +1,57 @@
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
-import "@fortawesome/fontawesome-free/css/all.min.css"
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import * as React from 'react';
-import {useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { AuthContext } from './account/AuthContext';
 import { useContext } from 'react';
 
-
-export default function Icon(){
-    
-    const router = useRouter()
+const Icon = () => {
+    const router = useRouter();
     const authContext = useContext(AuthContext);
-    const sss={
+
+    const sss = {
         display: 'flex',
-        paddingBottom:'5px'
-      }
-    const ssss={
-        paddingTop:'12px',
-        paddingLeft:'12px'
-    }
+        paddingBottom: '5px'
+    };
 
-    const sssss={
-        paddingTop:'12px',
-    }
-    
+    const ssss = {
+        paddingTop: '12px',
+        paddingLeft: '12px'
+    };
 
-    if(authContext.displayName!="" ){
-        if (authContext.displayName == "普通") {    
-            return(
-                <div  style={sss}>
-                <i className="fas fa-circle-user fa-3x" onClick={() => router.push("/account")}></i>
-                <h5 style={sssss}>使用者 {authContext.email}，您好</h5>
-                </div>
-            
-            )
+    const sssss = {
+        paddingTop: '12px',
+    };
+
+    // 只在客戶端執行的邏輯
+    if (typeof window !== 'undefined') {
+        if (authContext.displayName !== "") {
+            if (authContext.displayName === "普通") {
+                return (
+                    <div style={sss}>
+                        <i className="fas fa-circle-user fa-3x" onClick={() => router.push("/account")}></i>
+                        <h5 style={sssss}>使用者 {authContext.email}，您好</h5>
+                    </div>
+                );
+            } else {
+                return (
+                    <div style={sss}>
+                        <i className="fas fa-circle-user fa-3x" onClick={() => router.push("/account")}></i>
+                        <h5 style={ssss}>商家 {authContext.displayName} , 您好</h5>
+                    </div>
+                );
             }
-        else{
-            return(
+        } else {
+            return (
                 <div style={sss}>
-                <i className="fas fa-circle-user fa-3x" onClick={() => router.push("/account")}></i>
-                <h5 style={ssss}>商家 {authContext.displayName} , 您好</h5>
+                    <i className="fas fa-circle-user fa-3x" onClick={() => router.push("/account")}></i>
+                    <h5 style={ssss}>登入/註冊</h5>
                 </div>
-            )
+            );
         }
     }
-    
 
+    return null; // 避免在伺服器端返回非法內容
+};
 
-    else{
-        return(
-        <div style={sss}>
-                <i className="fas fa-circle-user fa-3x" onClick={() => router.push("/account")}></i>
-                <h5 style={ssss}>登入/註冊</h5>
-                </div>
-        )
-    }
-}
-
+export default Icon;

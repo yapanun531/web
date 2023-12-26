646f0792da52e54db3fcb20fc429b5b0e5380977
mnshm8231 <111236191+mnshm8231@users.noreply.github.com>
2023-12-27 00:53:23 +0800

    王子剛12:53

--- app/menu.tsx
+++ app/menu.tsx
@@ -40,15 +40,23 @@ export function Icon() {
           <h5 style={sssss}>使用者 {authContext.email}，您好</h5>
         </div>
       );
-    } else {
+    } else if(authContext && authContext.displayName === "管理員"){
       content = (
         <div style={sss}>
           <Link href="/account">
             <i className="fas fa-circle-user fa-3x"></i>
           </Link>
-          <h5 style={ssss}>商家 {authContext.displayName} , 您好</h5>
+          <h5 style={ssss}>管理員 , 您好</h5>
         </div>
       );
+    }
+    else{
+      <div style={sss}>
+      <Link href="/account">
+        <i className="fas fa-circle-user fa-3x"></i>
+      </Link>
+      <h5 style={ssss}>商家 {authContext.displayName} , 您好</h5>
+    </div>
     }
   } else {
     content = (
@@ -76,6 +84,8 @@ export function Menu() {
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+  
+  if(authContext.displayName!=""){
   if (authContext.displayName == "普通") {
     return (
 
@@ -98,6 +108,28 @@ export function Menu() {
 
       </div>
     );
+  }
+  else if(authContext.displayName == "管理員"){
+    return(
+      <div>
+        <Box sx={{ width: '100%' }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <TabList onChange={handleChange} aria-label="lab API tabs example">
+                <Tab label="誠園" onClick={() => router.push("/ChengYuanCustomer")} value="1" />
+                <Tab label="心園" onClick={() => router.push("/XinYuanCustomer")} value="2" />
+                <Tab label="理園" onClick={() => router.push("/LiYuanCustomer")} value="3" />
+                <Tab label="輔園" onClick={() => router.push("/FuYuanCustomer")} value="4" />
+                <Tab label="登入/註冊" onClick={() => router.push("/account")} value="5" />
+                <Tab label="帳號管理" onClick={() => router.push("/Menager")} value="6" />
+                <Tab label="排行榜" onClick={() => router.push("/RankingList")} value="7" />
+              </TabList>
+            </Box>
+          </TabContext>
+        </Box>
+
+      </div>
+    )
   }
   else {
     return (
@@ -122,4 +154,26 @@ export function Menu() {
       </div>
     );
   }
+}
+
+else{
+  return(
+    <div>
+        <Box sx={{ width: '100%' }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <TabList onChange={handleChange} aria-label="lab API tabs example">
+                <Tab label="誠園" onClick={() => router.push("/ChengYuan")} value="1" />
+                <Tab label="心園" onClick={() => router.push("/XinYuan")} value="2" />
+                <Tab label="理園" onClick={() => router.push("/LiYuan")} value="3" />
+                <Tab label="輔園" onClick={() => router.push("/FuYuan")} value="4" />
+                <Tab label="登入/註冊" onClick={() => router.push("/account")} value="5" />
+              </TabList>
+            </Box>
+          </TabContext>
+        </Box>
+
+      </div>
+  )
+}
 }
